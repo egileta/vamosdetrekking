59bbf783e6529dda22dedc1bfa3d786ea9ca1c1b
egileta <51084667+egileta@users.noreply.github.com>
2019-05-27 16:09:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="es-ES">
+
+<head>
+    <title>▷ Tu web de grupos para salidas al aire libre, trekking, senderismo...</title>
+    <meta name="robots" content="noindex, nofollow" /><!--TEMPORAL!! -->
+    <meta name="description" lang="en" content="Vamos de Trekking somos un grupo de entusiastas del trekking y la montaña que nos hemos propuesto ayudar a que la gente pueda crear grupos para salir al campo o montaña, a pasear, hacer trekking, nordic walk, running o el deporte de aventura que deseen">
+    <meta charset="utf-8" />
+    <meta name="author" content="Arturo Egileta" />
+    <meta name="viewport" content="width=device-width, maximum-scale=2.0">
+    <link rel="canonical" href="https://vamosdetrekking.es" />
+    <!-- <link rel="shortcut icon" href="favicon.ico" /> -->
+
+    <style type="text/css">
+        *       { box-sizing: border-box; }
+        body    {
+                    font-size: 1em;
+                    background-color: #F2F2F2;
+                    color: #1C3D73;
+                    font-family: verdana;
+                    text-align: center;
+                }
+        h1      { font-size: 1.2em; text-align: center; margin: 20px 0 20px 0; }
+        img     { display: block; margin: 0 auto; border: 0}
+        p       { padding: 10px; }
+        span    { display: block; }
+
+        @media screen and (min-width: 720px)
+        {
+                body { padding: 0 5%; }
+                img, p { display: inline-block; vertical-align: middle; }
+                p { width: calc(100% - 300px); padding: 0 20px; text-align: left; }
+                span { display: inline; }
+        }
+        @media screen and (min-width: 1200px)
+        {
+                body { padding: 0 20%; margin-top: 50px; }
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Crea tu grupo de trekking... <span>comienza a organizar salidas!</span></h1>
+    <!-- <div><a href="http://vestacp.com/">Powered by VESTA</a></div> -->
+    <img width="300" height="160" src="logo1.jpg" alt="logo vamosdetrekking.es"><p>Vamos de Trekking somos un grupo de entusiastas del trekking y la montaña que nos hemos propuesto ayudar a que la gente pueda crear grupos para salir al campo o montaña, a pasear, hacer trekking, nordic walk, running o el deporte de aventura que deseen
+    <br/><br/>Página web en construcción!</p>
+</body>
+
+</html>
